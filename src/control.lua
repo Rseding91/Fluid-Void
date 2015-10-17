d80b3a445cb83074861c66c0dde17df9679d48e3
Rseding91 <Rseding91@yahoo.com>
2015-10-17 15:54:05 -0400

    Updated for Factorio 0.12.11

--- src/control.lua
+++ src/control.lua
@@ -1,7 +1,5 @@
 require "defines"
 
-local loaded
-
 function ticker()
 	if global.pipes ~= nil then
 		if global.ticks == 0 or global.ticks == nil then
@@ -11,41 +9,29 @@ function ticker()
 			global.ticks = global.ticks - 1
 		end
 	else
-		game.on_event(defines.events.on_tick, nil)
+		script.on_event(defines.events.on_tick, nil)
 	end
 end
 
-game.on_load(function()
-	if not loaded then
-		loaded = true
-		
-		if global.pipes ~= nil then
-			game.on_event(defines.events.on_tick, ticker)
-		end
-	end
-end)
-
-game.on_init(function()
-	loaded = true
-	
-	if global.pipes ~= nil then
-		game.on_event(defines.events.on_tick, ticker)
-	end
+script.on_load(function()
+  if global.pipes ~= nil then
+    script.on_event(defines.events.on_tick, ticker)
+  end
 end)
 
 function builtEntity(event)
 	if event.created_entity.name == "void-pipe" then
 		if global.pipes == nil then
 			global.pipes = {}
-			game.on_event(defines.events.on_tick, ticker)
+			script.on_event(defines.events.on_tick, ticker)
 		end
 		
 		table.insert(global.pipes, event.created_entity)
 	end
 end
 
-game.on_event(defines.events.on_built_entity, builtEntity)
-game.on_event(defines.events.on_robot_built_entity, builtEntity)
+script.on_event(defines.events.on_built_entity, builtEntity)
+script.on_event(defines.events.on_robot_built_entity, builtEntity)
 
 function processPipes()
 	for k,pipe in pairs(global.pipes) do
